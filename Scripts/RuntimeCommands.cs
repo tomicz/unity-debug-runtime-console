1e8605a675e8dfcfd03565b00cfe3677be91d6e6
tomicz <darko_darkblack@yahoo.com>
2022-06-24 17:50:24 +0200

    Updated PrintList command. PrintList() didn't print END when failed to find list or when list doesn't contain any items.

--- Scripts/RuntimeCommands.cs
+++ Scripts/RuntimeCommands.cs
@@ -7,11 +7,12 @@ namespace TOMICZ.Debugger
     {
         public void PrintList(List<object> list)
         {
-            RuntimeConsole.PrintMessage(MessageType.Header, "Start - Printing list information");
+            RuntimeConsole.Header("Start - Printing list information");
 
             if (list == null)
             {
-                RuntimeConsole.PrintMessage(MessageType.Error, "List is null.");
+                RuntimeConsole.Error("List is null.");
+                RuntimeConsole.Header("End");
                 return;
             }
 
@@ -21,26 +22,28 @@ namespace TOMICZ.Debugger
                 foreach (object listItem in list)
                 {
                     counter++;
-                    RuntimeConsole.PrintMessage(MessageType.Log, "Itm:" + counter + " " + listItem.ToString());
+                    RuntimeConsole.Log("Itm:" + counter + " " + listItem.ToString());
                 }
 
-                RuntimeConsole.PrintMessage(MessageType.Log, "List length: " + list.Count);
+                RuntimeConsole.Log("List length: " + list.Count);
 
-                RuntimeConsole.PrintMessage(MessageType.Header, "End");
+                RuntimeConsole.Header("End");
             }
             else
             {
-                RuntimeConsole.PrintMessage(MessageType.Error, "Couldn't print list information because it's empty.");
+                RuntimeConsole.Error("Couldn't print list information because it's empty.");
+                RuntimeConsole.Header("End");
             }
         }
 
         public void PrintList(List<object> list, bool isSerialized)
         {
-            RuntimeConsole.PrintMessage(MessageType.Header, "Start - Printing list information");
+            RuntimeConsole.Header("Start - Printing list information");
 
             if (list == null)
             {
-                RuntimeConsole.PrintMessage(MessageType.Error, "List is null.");
+                RuntimeConsole.Error("List is null.");
+                RuntimeConsole.Header("End");
                 return;
             }
 
@@ -53,23 +56,24 @@ namespace TOMICZ.Debugger
 
                     if (!isSerialized)
                     {
-                        RuntimeConsole.PrintMessage(MessageType.Log, "Itm:" + counter + " " + listItem.ToString());
+                        RuntimeConsole.Log("Itm:" + counter + " " + listItem.ToString());
                     }
                     else
                     {
 
                         string json = JsonUtility.ToJson(listItem);
-                        RuntimeConsole.PrintMessage(MessageType.Log, "Itm:" + counter + " " + json);
+                        RuntimeConsole.Log("Itm:" + counter + " " + json);
                     }
                 }
 
-                RuntimeConsole.PrintMessage(MessageType.Log, "List length: " + list.Count);
+                RuntimeConsole.Log("List length: " + list.Count);
 
-                RuntimeConsole.PrintMessage(MessageType.Header, "End");
+                RuntimeConsole.Header("End");
             }
             else
             {
-                RuntimeConsole.PrintMessage(MessageType.Error, "Couldn't print list information because it's empty.");
+                RuntimeConsole.Header("Couldn't print list information because it's empty.");
+                RuntimeConsole.Header("End");
             }
         }
     }
