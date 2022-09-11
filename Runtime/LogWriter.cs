609166fad43be900a5e86659ac5c6e1d055106f5
tomicz <darko_darkblack@yahoo.com>
2022-09-11 16:38:46 +0200

    Added new class LogWriter.
    The new LogWriter is built on top of StreamWriter and its role is to log messages locally onto a device.

---
+++ Runtime/LogWriter.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using UnityEngine;
+
+namespace TOMICZ.Debugger
+{
+    public static class LogWriter
+    {
+        private static string path = Application.persistentDataPath + "/logs.txt";
+
+        /// <summary>
+        /// Logs a message to a persistent data path on your device.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogMessage(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// Clears existing logs.
+        /// </summary>
+        public static void ClearLogs()
+        {
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                writer.Write("");
+            }
+        }
+    }
+}
