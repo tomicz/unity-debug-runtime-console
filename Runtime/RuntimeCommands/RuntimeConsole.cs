b84df7a94d0a8c612a07935d592d47f30bd5d6eb
Darko Tomic <tomiczdarko@gmail.com>
2023-11-18 09:19:52 +0100

    Decoupled RuntimeConsole from ConsoleWIndow

--- Runtime/RuntimeCommands/RuntimeConsole.cs
+++ Runtime/RuntimeCommands/RuntimeConsole.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using TOMICZ.Debugger.DebugVisualisers;
-using UnityEngine;
 
 namespace TOMICZ.Debugger
 {
@@ -9,96 +6,28 @@ namespace TOMICZ.Debugger
     {
         public static Action OnUnityRunEvent;
 
-        public static Stack<DebugLineRenderer> DebugLineRenderers => _debugLineRenderes;
-
-        private static RuntimeCommands _runtimeCommands;
-        private static ConsoleWindow _consoleWindow;
-        private static LogWriter _logWriter;
-
-        private static List<ITick> _tickables = new List<ITick>();
-        private static Stack<DebugLineRenderer> _debugLineRenderes = new Stack<DebugLineRenderer>();
-
-        public static void Initilise(ConsoleWindow consoleWindow)
+        public static void Log(object log)
         {
-            _consoleWindow = consoleWindow;
-            _runtimeCommands = new RuntimeCommands();
-            _logWriter = new LogWriter();
-            _tickables.Add(_consoleWindow);
-            _logWriter.ClearLogs();
+
         }
 
-        public static void Log(object message)
+        public static void Warning(object log)
         {
-            _logWriter.Write(new LogMessage(LogMessageType.Log, message));
-            Tick();
+
         }
 
-        public static void Header(object message)
+        public static void Error(object log)
         {
-            _logWriter.Write(new LogMessage(LogMessageType.Header, message));
-            Tick();
-        }
 
-        public static void Error(object message)
-        {
-            _logWriter.Write(new LogMessage(LogMessageType.Error, message));
-            Tick();
         }
 
         public static void Loop(string message)
         {
-            _consoleWindow.Loop(message);
-            Tick();
+
         }
-
-        /// <summary>
-        /// Draw a debug line from pointA to pointB.
-        /// Make sure you call this method only once a frame (Awake(), Start(), OnEnable(), etc). Do not call it in an Update(), unless it is what you want.
-        /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="lineWidth"></param>
-        /// <param name="color"></param>
-        public static void DrawLine(Transform start, Transform end, float lineWidth, Color color)
-        {
-            DebugLineRenderer debugLineRenderer = new DebugLineRenderer(start, end, lineWidth, color);
-            _debugLineRenderes.Push(debugLineRenderer);
-        }
-
-        public static void PrintMessage(LogMessageType messageType, string message)
-        {
-            if (HasConsole())
-            {
-                WriteMessage(messageType, message);
-            }
-        }
-
-        public static void PrintList(List<object> list) => _runtimeCommands.PrintList(list);
-
-        public static void PrintList(List<object> list, bool isSerialized) => _runtimeCommands.PrintList(list, isSerialized);
 
         public static void RegisterEvent(Action action) => OnUnityRunEvent += action;
 
         public static void RemoveEvent(Action action) => OnUnityRunEvent -= action;
-
-        private static bool HasConsole()
-        {
-            if (_consoleWindow == null)
-            {
-                Debug.LogWarning("Runtime Console is missing console window. Create one inside Canvas with right-click > Tomicz > New Console Window.");
-                return false;
-            }
-            return true;
-        }
-
-        private static void WriteMessage(LogMessageType messageType, string message) => _consoleWindow.PrintMessage(new LogMessage(messageType, message));
-
-        private static void Tick()
-        {
-            foreach (var tick in _tickables)
-            {
-                tick.Tick();
-            }
-        }
     }
 }
